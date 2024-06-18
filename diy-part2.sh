181b05b6609dcd38c45146feb1ae6e565226c032
Pr0m1x <promix953@gmail.com>
2024-06-19 04:10:18 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,3 +14,14 @@ sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_gener
 #echo 'CONFIG_EXTRA_FIRMWARE="i915/skl_dmc_ver1_27.bin"' >> target/linux/x86/config-5.4
 #echo 'CONFIG_EXTRA_FIRMWARE_DIR="/lib/firmware"' >> target/linux/x86/config-5.4
 #echo -e 'CONFIG_DEVEL=y\nCONFIG_CCACHE=y' >> .config;
+
+#Patch luci-app-mwan3
+sed -i '15 a <script type="text/javascript" src="<%=resource%>/cbi.js"></script>' feeds/luci/applications/luci-app-mwan3/luasrc/view/mwan/status_detail.htm
+sed -i '28 a <script type="text/javascript" src="<%=resource%>/cbi.js"></script>' feeds/luci/applications/luci-app-mwan3/luasrc/view/mwan/status_diagnostics.htm
+sed -i '15 a <script type="text/javascript" src="<%=resource%>/cbi.js"></script>' feeds/luci/applications/luci-app-mwan3/luasrc/view/mwan/status_interface.htm
+sed -i '15 a <script type="text/javascript" src="<%=resource%>/cbi.js"></script>' feeds/luci/applications/luci-app-mwan3/luasrc/view/mwan/status_troubleshooting.htm
+
+cat feeds/luci/applications/luci-app-mwan3/luasrc/view/mwan/status_detail.htm
+cat feeds/luci/applications/luci-app-mwan3/luasrc/view/mwan/status_diagnostics.htm
+cat feeds/luci/applications/luci-app-mwan3/luasrc/view/mwan/status_interface.htm
+cat feeds/luci/applications/luci-app-mwan3/luasrc/view/mwan/status_troubleshooting.htm
