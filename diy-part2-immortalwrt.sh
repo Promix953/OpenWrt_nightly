255431ab411e4c62e70022f8f263d1efcb45e4ab
Pr0m1x <promix953@gmail.com>
2025-10-02 20:27:28 +0800

    移除构建脚本中的intel-microcode补丁应用步骤

--- diy-part2-immortalwrt.sh
+++ diy-part2-immortalwrt.sh
@@ -28,6 +28,3 @@ sed -i '/CONFIG_KERNEL_BUILD_USER/d' .config &&
 # Add kernel build domain
 sed -i '/CONFIG_KERNEL_BUILD_DOMAIN/d' .config &&
     echo 'CONFIG_KERNEL_BUILD_DOMAIN="GitHub Actions"' >>.config
-
-git apply fix_iucode.patch &&
-    echo 'Apply intel-microcode patch'
